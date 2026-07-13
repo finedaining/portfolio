203908316806fd6254df523193cd533c7c255db3
finedaining <kmm0012@gmail.com>
2026-07-13 20:52:11 +0900

    이노벨2section

--- src/projects/InobelWebsite.jsx
+++ src/projects/InobelWebsite.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import './InobelWebsite.css';
 
-const logo      = "/images/inovel/logo.svg";
+const logo        = "/images/inovel/logo.svg";
+const s2Phones    = "/images/inovel/s2-phones.png";
+const s2Web       = "/images/inovel/s2-web.png";
+const s2Bestseller = "/images/inovel/s2-bestseller.png";
 const bgEclipse = "/images/inovel/bg-eclipse.svg";
 const glassBig  = "/images/inovel/glass-big.svg";
 const glassMini = "/images/inovel/glass-mini.svg";
@@ -49,6 +52,44 @@ export default function InobelWebsite({ onBack }) {
           </div>
         </div>
 
+      </section>
+
+      {/* ── 2. 웹 디자인 ── */}
+      <section className="inov__s2">
+        <div className="inov__s2-left">
+          <img src={logo} alt="innovelle" className="inov__s2-logo" />
+          <img src={s2Phones} alt="" className="inov__s2-phones" />
+          <div className="inov__s2-text">
+            <h2 className="inov__s2-title">이노벨 pc/mobile web</h2>
+            <p className="inov__cover-desc">
+              메인 컬러인 주황색을 적극 활용해 브랜드 아이덴티티를 확고히 했으며,<br />
+              '이노벨' 브랜드에 걸맞은 혁신적이고 에너제틱한 이미지를 표현하고자 했습니다.<br />
+              또한 깔끔하고 모던한 느낌을 강조하고, '클린 뷰티, 퓨어 뷰티'에 어울리는 분위기에<br />
+              맞춰 이노벨 패키지 목업 이미지를 함께 활용해 전체 디자인의 완성도를 높였습니다.
+            </p>
+            <p className="inov__cover-desc">
+              모바일에서는 PC 버전의 구성을 유지하되, 작은 화면에서도 가독성과 사용 편의성을<br />
+              높이기 위해 보다 직관적이고 터치 중심의 사용성을 반영한 UI로 구성하였습니다.
+            </p>
+            <div className="inov__badge">
+              <div className="inov__badge-bar-wrap">
+                <div className="inov__badge-bar">
+                  <div className="inov__badge-fill inov__badge-fill--30" />
+                  <div className="inov__badge-dot inov__badge-dot--30" />
+                </div>
+                <span className="inov__badge-label">피그마, 기여도 30%</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="inov__s2-web">
+          <img src={s2Web} alt="이노벨 pc 웹" />
+        </div>
+
+        <div className="inov__s2-right">
+          <img src={s2Bestseller} alt="이노벨 베스트셀러" />
+        </div>
       </section>
     </div>
   );
